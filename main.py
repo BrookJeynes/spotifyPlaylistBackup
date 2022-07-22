8259acdf1b76abd8c2c56fba98ce7fe671a163db
Brook <jeynesbrook@gmail.com>
2022-07-22 16:22:59 +1000

    wip - code cleanup

--- main.py
+++ main.py
@@ -1,31 +1,10 @@
-# ----------------------------#
-#            CREDIT           #
-# BROOK JEYNES + BRADY STROUD #
-#-----------------------------#
+# Credits: Brook Jeynes & Brady Stroud
 
-#----------------------------------------#
-# TO DO:                                 #
-# - IMPORT FILE DATA TO SPOTIFY PLAYLIST #
-#    - CREATE NEW USER PLAYLIST          #
-#    - READ EACH URI FROM FILE           #
-#    - ADD URI TO PLAYLIST               #
-#----------------------------------------#
-
-
-#-----------#
-# VARIABLES #
-#-----------#
 import os
 import json
 import spotipy
 import spotipy.util as util
 
-
-#-----------#
-# FUNCTIONS #
-#-----------#
-
-# INITIALISES EVERYTHING
 def init():
     username = "username_here"
     scope = 'user-read-private user-read-playback-state user-modify-playback-state'
@@ -44,32 +23,26 @@ def init():
     spotifyObject = spotipy.Spotify(auth=token)
 
 
-# GRABS THE NAME OF SONG AND SONG CODE AND OUTPUTS IT TO A FILE
 def backup():
     code = input("code: ")
     fileName = input("file name: ")
-    length = spotifyObject.playlist_tracks(code, fields="total", offset=0, market=None, additional_types=('track', ))["total"] # GRABS LENGTH OF PLAYLIST
-
-    loopLimit = int(length / 100) # DECIDES HOW MANY TIMES TO LOOP DUE TO LIMIT OF 100 SONGS PER REQUEST
+    length = spotifyObject.playlist_tracks(code, fields="total", offset=0, market=None, additional_types=('track', ))["total"] 
+    # caps each loop to 100 songs due to spotify limit
+    loopLimit = int(length / 100)
     offsetCounter = 0 
 
     for _ in range(loopLimit + 1):
         playlist = spotifyObject.playlist_tracks(code, fields="items(track(name, uri))", offset=offsetCounter, market=None, additional_types=('track', )) # GRAB SPOTIFY SONG TITLE AND URI
  
-        f = open((fileName + ".txt"), "a") # OUTPUTS DATA TO FILE
+        f = open((fileName + ".txt"), "a") 
         f.write(json.dumps(playlist, indent=4)) # FORMATS DATA
         f.close()
 
-        offsetCounter += 100 # INCREMENTS OFFSET COUNTER
+        offsetCounter += 100 
 
-# MAIN FUNCTION
 def main():
-    init()
-    backup()
+  init()
+  backup()
 
-
-#--------------#
-# MAIN PROGRAM #
-#--------------#
 if __name__ == "__main__":
-    main()
+  main()
